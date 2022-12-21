b048731d0dc4fa830010a6a6906692f54a1be33c
Nathan Lum <nathan.lum@ucalgary.ca>
2022-12-20 22:06:32 -0700

    Update Rotating display.ino

--- Rotating display.ino
+++ Rotating display.ino
@@ -12,11 +12,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   for (int i = 0; i < 180; i++) {
     servo.write(i);
-    delay(15);
+    delay(70);
   }
 
   for (int i = 180; i > 0; i--) {
     servo.write(i);
-    delay(15);
+    delay(70);
   }
 } 
